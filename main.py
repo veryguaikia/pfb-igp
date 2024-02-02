3dc1229781f8d56a6c3d4085a097fa6e4459af98
Backuvu <156386587+Backuvu@users.noreply.github.com>
2024-02-02 13:12:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from pathlib import Path
 import cash_on_hand, profit_loss, overheads
 
 overheads.find_highest_overhead(Path(r"csv_reports\Overheads.csv"))
-cash_on_hand.analyze_cash_on_hand(Path(r"csv_reports\Cash-on-Hand.csv"))
-profit_loss.analyze_netprofit(Path(r"csv_reports\Profit-and-Loss.csv"))
+cash_on_hand.analyze_cash_on_hand(Path(r"csv_reports\Cash_on_Hand.csv"))
+profit_loss.analyze_netprofit(Path(r"csv_reports\Profits_and_Loss.csv"))
